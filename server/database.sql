dd66f5971f6adf2bba81d0d7ba67a550f4e25756
martin <martin@sensorik-debian-10.4.0>
2020-09-26 14:01:19 +0200

    Added commands for creating the database

--- server/database.sql
+++ server/database.sql
@@ -3,6 +3,21 @@ CREATE DATABASE sponeo;
 /* Noch erledigen */
 CREATE TABLE websites (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(50),
+  institution VARCHAR(64),
+  homepage VARCHAR(128),
+  newspage VARCHAR(128),
+  category VARCHAR(32),
+  sports VARCHAR(32),
+  geo VARCHAR(32),
+  article_container VARCHAR(32),
+  headline_element VARCHAR(32),
+  href_element VARCHAR(32)
+)
 
+CREATE TABLE articles (
+  id SERIAL PRIMARY KEY,
+  institution VARCHAR(64),
+  homepage VARCHAR(128),
+  url VARCHAR(128),
+  headline VARCHAR(64)
 )
