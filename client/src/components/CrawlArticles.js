36b1dcbe4e14dc24e81bbbc24db801e012836e49
martin <martin@sensorik-debian-10.4.0>
2020-10-05 18:26:04 +0200

    Added templates for the crawler page and the page that displays the articles

---
+++ client/src/components/CrawlArticles.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+
+class CrawlArticles extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <Fragment>
+        <h1>CrawlArticles</h1>
+      </Fragment>
+    )
+  }
+}
+
+export default CrawlArticles;
