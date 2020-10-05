6d4a6b98e92601f91f9e6d0cb279200cf300f6f2
martin <martin@sensorik-debian-10.4.0>
2020-10-05 18:24:04 +0200

    Added navbar and functionality of navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,43 @@ import React, { Fragment } from 'react';
 import './App.css';
 
 //Components
+import Navbar from './components/Navbar.js';
 import AddWebsite from './components/AddWebsite.js';
+import CrawlArticles from './components/CrawlArticles.js';
+import DisplayArticles from './components/DisplayArticles.js';
 
 class App extends React.Component{
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      page: 'addWebsite',
+    };
+
+    this.pageHandler = this.pageHandler.bind(this);
   }
 
+  pageHandler = (e) => {
+    this.setState({
+      page: e.target.getAttribute('name'),
+    })
+  }
 
   render() {
+    let currentPage;
+
+    if(this.state.page === 'addWebsite') {
+      currentPage = <AddWebsite />;
+    } else if(this.state.page === 'crawlArticles') {
+      currentPage = <CrawlArticles />;
+    } else if(this.state.page === 'displayArticles') {
+      currentPage = <DisplayArticles />;
+    }
+
+
     return(
       <Fragment>
-        <div></div>
-        <AddWebsite />
+        <Navbar pageHandler={this.pageHandler}/>
+        {currentPage}
       </Fragment>
     )
   }
