dec6bec545ca943eab09af66a83c01027825e403
PfMartin <martinpfatrisch@gmail.com>
2021-01-29 17:08:29 +0100

    Changed the landing page of the app

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      page: 'showWebsites',
+      page: 'addWebsite',
     };
 
     this.pageHandler = this.pageHandler.bind(this);
